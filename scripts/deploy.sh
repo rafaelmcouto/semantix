7f3c15dc0cadb307d41e00ae019500e24f49a46d
rafael <rafaelmcouto@gmail.com>
2019-07-02 13:25:32 -0300

    Just a line

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,9 @@
 ###  Deploying the Image  ###
 #############################
 
+echo "Stopping the container if exists"
 docker stop api_semantix
-docker run -d -p 4000:8888 --rm --network semantix_semantix --name api_semantix rafaelmcouto/semantix_python:0.1 
+docker run -d -p 4000:8888 --rm --name api_semantix --network semantix_semantix rafaelmcouto/semantix_python:0.1 
 
 sleep 5
 
